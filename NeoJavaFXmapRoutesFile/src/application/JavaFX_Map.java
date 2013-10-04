0352f4a9e59f75b1b47d44a5ae26fafb5f3e454c
Eduardo Esparza Zamarripa <neoraidenx@gmail.com>
2013-10-04 01:56:49 -0500

    Add project

---
+++ NeoJavaFXmapRoutesFile/src/application/JavaFX_Map.java
@@ -0,0 +1,77 @@
+package application;
+	
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import java.io.File;
+import java.net.MalformedURLException;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Region;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
+
+
+public class JavaFX_Map extends Application {
+	
+	private Scene scene;
+	MyBrowser myBrowser;
+	
+	public JavaFX_Map(){
+		myBrowser = new MyBrowser();
+	}
+	
+	@Override
+	public void start(Stage primaryStage) {
+		primaryStage.setTitle("NeoMapUbication");
+		
+		try {
+			scene = new Scene(myBrowser,640,480);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.show();
+			myBrowser.reload();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+	
+	class MyBrowser extends Region{
+		HBox toolbar;
+		
+		WebView webView = new WebView();
+		WebEngine webEngine = webView.getEngine();
+		
+		public MyBrowser(){
+			String desktopPath = desktopPath();
+			File f = new File(desktopPath);
+			try {
+				webEngine.load(f.toURI().toURL().toString());
+			} catch (MalformedURLException e) {
+				e.printStackTrace();
+			}
+			getChildren().add(webView);
+	}
+		public void reload(){
+			webEngine.reload();
+		}
+		
+		public String desktopPath(){
+			String desktopPath = "";
+			try{
+				desktopPath = System.getProperty("user.home") + "/Desktop/googlemaps.html";
+				desktopPath = desktopPath.replace("\\", "/");
+				System.out.println(desktopPath);
+			}catch(Exception e){
+				e.printStackTrace();
+			}
+			return desktopPath;
+		}
+		
+	}
+	
+	
+}
---
+++ NeoJavaFXmapRoutesFile/src/application/Main.java
@@ -0,0 +1,138 @@
+package application;
+import java.io.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+import javafx.scene.layout.VBoxBuilder;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+ 
+public class Main extends Application {
+ 
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+    String coor = "";
+    int numOfCoor = 0;
+    String desktopPath = "";
+     
+    @Override
+    public void start(final Stage primaryStage) {
+        primaryStage.setTitle("File Chooser");
+        Group root = new Group();
+        
+         
+        Button buttonLoad = new Button("Cargar Archivo");
+        buttonLoad.setOnAction(new EventHandler<ActionEvent>(){
+ 
+            @Override
+            public void handle(ActionEvent arg0) {
+                FileChooser fileChooser = new FileChooser();
+                 
+                //Set extension filter
+                FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("TXT files (*.txt)", "*.txt");
+                fileChooser.getExtensionFilters().add(extFilter);
+                 
+                //Show save file dialog
+                File file = fileChooser.showOpenDialog(primaryStage);
+                if(file != null){
+                    coor = readFile(file);
+                    File html = new File("googlemapsTemplate.html");
+            		String code = readFileHtml(html);
+            		editFile(code,coor);
+            		System.out.println("Cargado");
+                    JavaFX_Map map = new JavaFX_Map();
+                    map.start(primaryStage);
+                }
+            }
+             
+        });
+         
+        VBox vBox = VBoxBuilder.create()
+                .children(buttonLoad)
+                .build();
+         
+        root.getChildren().add(vBox);
+        primaryStage.setScene(new Scene(root, 200, 100));
+        primaryStage.show();
+    }
+     
+    private String readFile(File file){
+        StringBuilder stringBuffer = new StringBuilder();
+        BufferedReader bufferedReader = null;
+         
+        try {
+ 
+            bufferedReader = new BufferedReader(new FileReader(file));
+             
+            String text;
+            while ((text = bufferedReader.readLine()) != null) {
+                stringBuffer.append("new google.maps.LatLng(" + text + ")," + "\n");
+                numOfCoor++;
+            }
+ 
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            try {
+                bufferedReader.close();
+            } catch (IOException ex) {
+                Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        } 
+         
+        return stringBuffer.toString();
+    }
+    
+    private void editFile(String codeToFile, String coorToPush){
+		File openToEdit = new File("googlemaps.html");
+		try {
+			FileWriter editor = new FileWriter(openToEdit,false);
+			String htmlCode = codeToFile.replace("//CoordenadasStart", coorToPush);
+			editor.write(htmlCode);
+			editor.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+    
+    private String readFileHtml(File file){
+        StringBuilder stringBuffer = new StringBuilder();
+        BufferedReader bufferedReader = null;
+         
+        try {
+ 
+            bufferedReader = new BufferedReader(new FileReader(file));
+             
+            String text;
+            while ((text = bufferedReader.readLine()) != null) {
+                stringBuffer.append(text + "\n");
+            }
+ 
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            try {
+                bufferedReader.close();
+            } catch (IOException ex) {
+                Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        } 
+         
+        return stringBuffer.toString();
+    }
+}
